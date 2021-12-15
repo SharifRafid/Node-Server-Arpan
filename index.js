8eff854741cd5d5a6b8ec4adb580966e945c49dc
Sharif Rafid Ur Rahman <sharifrafidurrahman@gmail.com>
2021-12-15 09:02:09 +0600

    Fixed Admin Notification Page Click Issue

--- index.js
+++ index.js
@@ -529,7 +529,7 @@ app.post('/send-notification-to-admin-app-about-a-new-order-that-he-recieved', a
   const apititle = req.body.apititle
   const apibody = req.body.apibody
   const orderID = req.body.orderID
-  const click_action = ".ui.home.HomeActivity"
+  const click_action = ".ui.home.HomeActivityMain"
   
   let userToken = []
 
